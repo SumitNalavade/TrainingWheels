5484c09ee71da05cfe6e117476cb4b4994364a3d
Sumit Nalavade <vs.nalavade2003@gmail.com>
2024-11-02 16:04:23 -0500

    Organize code

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,9 @@ from dotenv import load_dotenv
 
 from database.database import db
 
-from chains.conversational_retrieval_chain_with_memory import build_conversational_retrieval_chain_with_memory
-from langchain.chat_models import ChatOpenAI
-from database.pg_vector_store import build_pg_vector_store
-from embeddings.openai_embeddings import openai_embeddings
-
 from routes.auth_routes import auth_routes_bp
 from routes.processing_routes import processing_routes_bp
+from routes.rag_routes import rag_routes_bp
 
 load_dotenv()
 
@@ -34,53 +30,15 @@ with app.app_context():
     db.init_app(app)
     db.create_all()
 
-llm = ChatOpenAI()
-collection_name = "test"
-pg_vector_store = build_pg_vector_store(embeddings_model=openai_embeddings, collection_name=collection_name, connection_uri=database_uri)
-pg_vector_retriever = pg_vector_store.as_retriever(search_type="mmr")
-
-retrieval_qa_chain = build_conversational_retrieval_chain_with_memory(
-        llm, pg_vector_retriever, '123')
-
 # Basic hello world route
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/search", methods=['POST'])
-def search():
-    data = request.get_json()
-    user_id = data.get('user_id')
-    query = data.get("query")
-    conversation_id = data.get("conversation_id")
-
-    # Build the vector store and retriever
-    collection_name = user_id
-    pg_vector_store = build_pg_vector_store(embeddings_model=openai_embeddings, collection_name=collection_name, connection_uri=database_uri)
-    pg_vector_retriever = pg_vector_store.as_retriever(search_type="mmr")
-
-    # Create the retrieval QA chain
-    retrieval_qa_chain = build_conversational_retrieval_chain_with_memory(llm, pg_vector_retriever, conversation_id)
-
-    # Run the query
-    result = retrieval_qa_chain.run(query)
-
-    # Construct the response dictionary
-    response = {
-        "session_id": conversation_id,
-        "message": {
-            "type": "ai",
-            "data": {
-                "content": result
-            }
-        }
-    }
-
-    return jsonify(response)
-
 def register_blueprints(app):
     app.register_blueprint(auth_routes_bp)
     app.register_blueprint(processing_routes_bp)
+    app.register_blueprint(rag_routes_bp)
 
 register_blueprints(app)
 
