8673fb206840e6081076695b0785d45f33d71032
Sumit Nalavade <vs.nalavade2003@gmail.com>
2024-11-02 14:35:27 -0500

    Full stack auth works

--- frontend/src/pages/StudioPage.tsx
+++ frontend/src/pages/StudioPage.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import useAppStore from "../stores/useAppStore";
 
 const StudioPage: React.FC = () => {
+    const user = useAppStore(state => state.user);
+    
     return (
-        <h1>Studio Page</h1>
+        <>
+            <h1>Studio Page</h1>
+
+            <p>{user?.name}</p>
+        </>
     )
 }
 
